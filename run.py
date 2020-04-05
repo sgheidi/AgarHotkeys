5a694b5b5a55a48873233c405d6a4ea38a330a12
Shervan Gheidi <shergheidi@gmail.com>
2020-04-04 19:49:46 -0700

    AgarHotkeys

---
+++ run.py
@@ -0,0 +1,22 @@
+from pynput.keyboard import Key, Listener
+import pyautogui
+from win32api import GetSystemMetrics
+
+def on_press(key):
+    print('{0} pressed'.format(
+        key))
+    if key == Key.shift_l:
+      pyautogui.moveTo((GetSystemMetrics(0)//2)+1, (GetSystemMetrics(1)//2))
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
